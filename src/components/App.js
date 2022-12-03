a27db640bb99fa694cc1a97c2c509c85c0bc5c7a
halilibrahimcelik <halil.celik139@gmail.com>
2022-12-03 15:43:17 +0300

    finished the assignment.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,31 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  const fetchData = async () => {
+    try {
+      const params =
+        filters.type === "all"
+          ? "pets"
+          : filters.type === "cat"
+          ? "pets?type=cat"
+          : filters.type === "dog"
+          ? "pets?type=dog"
+          : filters.type === "micropig"
+          ? "pets?type=micropig"
+          : "all";
+
+      const URL = `http://localhost:3001/${params}`;
+
+      const response = await fetch(URL);
+      const data = await response.json();
+      setPets(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="ui container">
@@ -15,10 +40,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters setFilters={setFilters} fetchData={fetchData} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} setPets={setPets} />
           </div>
         </div>
       </div>
